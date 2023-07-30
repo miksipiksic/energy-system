098dc214ca047d0e7ec20555d3afbcceabfd175c
miksipiksic <88499004+miksipiksic@users.noreply.github.com>
2023-07-30 09:10:25 +0200

    Update VodenaPovrs.java

--- VodenaPovrs.java
+++ VodenaPovrs.java
@@ -2,7 +2,7 @@ package energetika;
 
 import java.awt.Color;
 
-public class VodenaPovrs extends Parcela {
+public class VodenaPovrs extends Parcela { // water surface
 	
 	public VodenaPovrs() {
 		super("~", Color.CYAN);
