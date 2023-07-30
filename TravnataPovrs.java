04214e697668f8d9f0154584393c5630d7e663af
miksipiksic <88499004+miksipiksic@users.noreply.github.com>
2023-07-30 09:10:05 +0200

    Update TravnataPovrs.java

--- TravnataPovrs.java
+++ TravnataPovrs.java
@@ -2,10 +2,9 @@ package energetika;
 
 import java.awt.Color;
 
-public class TravnataPovrs extends Parcela {
+public class TravnataPovrs extends Parcela { // Grass surface
 
 	public TravnataPovrs() {
 		super("*", Color.GREEN);
-		// TODO Auto-generated constructor stub
 	}
 }
